13d699c8c12ad52d8f170c3d38f9a809961d8013
Ahmed Galal <AhmedGalal10895@Gmail.com>
2023-02-25 01:09:46 +0200

    Set dynamic port details in response

--- src/main/java/com/microservices/currencyexchangeservice/model/CurrencyExchange.java
+++ src/main/java/com/microservices/currencyexchangeservice/model/CurrencyExchange.java
@@ -1,7 +1,6 @@
 package com.microservices.currencyexchangeservice.model;
 
 import com.microservices.currencyexchangeservice.enums.Currency;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,10 +10,19 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class CurrencyExchange {
     private Long id;
     private Currency givenCurrency;
     private Currency targetCurrency;
     private BigDecimal conversionMultiple;
+    private String environment;
+
+    public CurrencyExchange(Long id, Currency givenCurrency, Currency targetCurrency, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.givenCurrency = givenCurrency;
+        this.targetCurrency = targetCurrency;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+
 }
