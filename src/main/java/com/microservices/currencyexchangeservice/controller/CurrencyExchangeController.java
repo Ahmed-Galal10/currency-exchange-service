52d291e100a95b6b893c51f383748b70811faa0d
Ahmed Galal <AhmedGalal10895@Gmail.com>
2023-02-25 01:10:01 +0200

    Set dynamic port details in response

--- src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.microservices.currencyexchangeservice.controller;
 
 import com.microservices.currencyexchangeservice.enums.Currency;
 import com.microservices.currencyexchangeservice.model.CurrencyExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +14,16 @@ import java.math.BigDecimal;
 @RestController
 @RequestMapping("/currency-exchange")
 public class CurrencyExchangeController {
+    
+    @Autowired
+    private Environment environment;
 
     @GetMapping("/from/{givenCurrency}/to/{targetCurrency}")
     public CurrencyExchange getExchangeValue(@PathVariable String givenCurrency,
                                              @PathVariable String targetCurrency){
-        return new CurrencyExchange(1L, Currency.USD, Currency.EGP, BigDecimal.valueOf(30));
+        CurrencyExchange currencyExchange = new CurrencyExchange(1L, Currency.USD, Currency.EGP, BigDecimal.valueOf(30));
+        String port = environment.getProperty("server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
     }
 }
