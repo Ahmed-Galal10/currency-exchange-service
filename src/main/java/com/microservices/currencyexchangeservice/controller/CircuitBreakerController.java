042afb9e45b963da43a1ab3ccd0dad9cf25117a3
Ahmed Galal <AhmedGalal10895@Gmail.com>
2023-02-26 03:26:06 +0200

    Try different config with @Retry to recall after failing

--- src/main/java/com/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,13 +1,32 @@
 package com.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 public class CircuitBreakerController {
 
+    private final Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+    static int wowCounter = 0;
+
+    @Retry(name = "wow", fallbackMethod = "fallbackResponse")
     @GetMapping("/wow")
-    public String wow(){
-        return "WoOoOoOoOoW";
+    public String wow() {
+        logger.info("wow endpoint is called {} times", ++wowCounter);
+
+        ResponseEntity<String> responseEntity = new RestTemplate()
+                .getForEntity("http://localhost:8080/fake-url", String.class);
+
+        return responseEntity.getBody();
+    }
+
+    public String fallbackResponse(Exception ex){
+        return "There is an error happened ):";
     }
 }
