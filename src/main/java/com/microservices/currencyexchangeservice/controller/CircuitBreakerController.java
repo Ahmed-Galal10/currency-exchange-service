c41e7624329cc006cce1f6d364bc59e9ead2898d
Ahmed Galal <AhmedGalal10895@Gmail.com>
2023-02-26 03:49:38 +0200

    Try @RateLimiter with configuring limit-for-period & limit-refresh-period

--- src/main/java/com/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,12 +1,10 @@
 package com.microservices.currencyexchangeservice.controller;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 
 @RestController
@@ -15,16 +13,18 @@ public class CircuitBreakerController {
     private final Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     static int wowCounter = 0;
 
-    //    @Retry(name = "wow", fallbackMethod = "fallbackResponse")
-    @CircuitBreaker(name = "default", fallbackMethod = "fallbackResponse")
+    // @Retry(name = "wow", fallbackMethod = "fallbackResponse")
+    // @CircuitBreaker(name = "default", fallbackMethod = "fallbackResponse")
+    @RateLimiter(name = "default")
+    // allow maximum 3 calls per 10 seconds
     @GetMapping("/wow")
     public String wow() {
         logger.info("wow endpoint is called {} times", ++wowCounter);
 
-        ResponseEntity<String> responseEntity = new RestTemplate()
-                .getForEntity("http://localhost:8080/fake-url", String.class);
-
-        return responseEntity.getBody();
+//        ResponseEntity<String> responseEntity = new RestTemplate()
+//                .getForEntity("http://localhost:8080/fake-url", String.class);
+//        return responseEntity.getBody();
+        return "WoOoOoW";
     }
 
     public String fallbackResponse(Exception ex) {
